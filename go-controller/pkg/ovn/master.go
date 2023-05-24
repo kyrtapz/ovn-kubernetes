kyrtapz/ovn-kubernetes

commit 8aa14239e4b12966352d903253005fe07b15b1e2

network-controller-manager: Create interconnect resources.
This patch creates interconnect resources in the Northbound
database to inter-connect the configured zones if interconnect
feature is enabled.

ovnkube network controller manager  is expected to run on each
configured zone.  Network controller manager  does the similar job
to that of the `ovn-ic` service provided by OVN.  An interconnect
Transit switch is created and it is connected to the OVN cluster router.

For each local node of a zone
  - a pair of logical switch port - logical router port
    is created. Logical switch port is created on the Transit switch
    and the router port is created on the OVN cluster router.

For each remote node (not part of the local zone)
  - a remote chassis entry is created in the Southbound database
  - a remote port is created in the Transit switch and this remote
port is bound to the remote chassis.
  - A static route is added in the OVN cluster router to send the
    traffic to the Transit switch if the packet is destined to
    the remote node.

This facilitates the packet traversal from a local zone node to a
remote zone node.  When the packet is received by the remote node,
it resumes the egress stage of the Transit switch.  From the
Transit switch the packet enters the OVN cluster router and then
to the node logical switch and finally to the destination pod.

Inter connection is presently restricted to the default network.

With this patch, multiple zones are supported for the default network
with few other features missing. Upcoming commits will add that support.

Co-authored-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

go-controller/pkg/ovn/master.go
@@ -466,7 +466,8 @@ func (oc *DefaultNetworkController) deleteStaleNodeChassis(node *kapi.Node) erro
 	return nil
 }
 
-func (oc *DefaultNetworkController) deleteNode(nodeName string) error {
+// cleanupNodeResources deletes the node resources from the OVN Northbound database
+func (oc *DefaultNetworkController) cleanupNodeResources(nodeName string) error {
 	if err := oc.deleteNodeLogicalNetwork(nodeName); err != nil {
 		return fmt.Errorf("error deleting node %s logical network: %v", nodeName, err)
 	}
@@ -603,8 +604,8 @@ func (oc *DefaultNetworkController) syncNodes(kNodes []interface{}) error {
 	}
 	for _, nodeSwitch := range nodeSwitches {
 		if !foundNodes.Has(nodeSwitch.Name) {
-			if err := oc.deleteNode(nodeSwitch.Name); err != nil {
-				return fmt.Errorf("failed to delete node:%s, err:%v", nodeSwitch.Name, err)
+			if err := oc.cleanupNodeResources(nodeSwitch.Name); err != nil {
+				return fmt.Errorf("failed to cleanup node resources:%s, err:%v", nodeSwitch.Name, err)
 			}
 		}
 	}
@@ -612,6 +613,17 @@ func (oc *DefaultNetworkController) syncNodes(kNodes []interface{}) error {
 	if err := oc.syncChassis(localZoneKNodes, remoteZoneKNodes); err != nil {
 		return fmt.Errorf("failed to sync chassis: error: %v", err)
 	}
+
+	if config.OVNKubernetesFeature.EnableInterconnect {
+		if err := oc.zoneChassisHandler.SyncNodes(kNodes); err != nil {
+			return fmt.Errorf("zoneChassisHandler failed to sync nodes: error: %w", err)
+		}
+
+		if err := oc.zoneICHandler.SyncNodes(kNodes); err != nil {
+			return fmt.Errorf("zoneICHandler failed to sync nodes: error: %w", err)
+		}
+	}
+
 	return nil
 }
 
@@ -709,6 +721,7 @@ type nodeSyncs struct {
 	syncMgmtPort          bool
 	syncGw                bool
 	syncHo                bool
+	syncZoneIC            bool
 }
 
 func (oc *DefaultNetworkController) addUpdateLocalNodeEvent(node *kapi.Node, nSyncs *nodeSyncs) error {
@@ -742,6 +755,9 @@ func (oc *DefaultNetworkController) addUpdateLocalNodeEvent(node *kapi.Node, nSy
 			oc.mgmtPortFailed.Store(node.Name, true)
 			oc.gatewaysFailed.Store(node.Name, true)
 			oc.hybridOverlayFailed.Store(node.Name, config.HybridOverlay.Enabled)
+			if nSyncs.syncZoneIC {
+				oc.syncZoneICFailed.Store(node.Name, true)
+			}
 			err = fmt.Errorf("nodeAdd: error adding node %q: %w", node.Name, err)
 			oc.recordNodeErrorEvent(node, err)
 			return err
@@ -822,26 +838,67 @@ func (oc *DefaultNetworkController) addUpdateLocalNodeEvent(node *kapi.Node, nSy
 		}
 	}
 
+	if nSyncs.syncZoneIC && config.OVNKubernetesFeature.EnableInterconnect {
+		// Call zone chassis handler's AddLocalZoneNode function to mark
+		// this node's chassis record in Southbound db as a local zone chassis.
+		// This is required when a node moves from a remote zone to local zone
+		if err := oc.zoneChassisHandler.AddLocalZoneNode(node); err != nil {
+			errs = append(errs, err)
+			oc.syncZoneICFailed.Store(node.Name, true)
+		} else {
+			// Call zone IC handler's AddLocalZoneNode function to create
+			// interconnect resources in the OVN Northbound db for this local zone node.
+			if err := oc.zoneICHandler.AddLocalZoneNode(node); err != nil {
+				errs = append(errs, err)
+				oc.syncZoneICFailed.Store(node.Name, true)
+			} else {
+				oc.syncZoneICFailed.Delete(node.Name)
+			}
+		}
+	}
+
 	err = kerrors.NewAggregate(errs)
 	if err != nil {
 		oc.recordNodeErrorEvent(node, err)
 	}
+
 	return err
 }
 
-func (oc *DefaultNetworkController) addUpdateRemoteNodeEvent(node *kapi.Node) error {
+func (oc *DefaultNetworkController) addUpdateRemoteNodeEvent(node *kapi.Node, syncZoneIC bool) error {
 	// Check if the remote node is present in the local zone nodes.  If its present
 	// it means it moved from this controller zone to other remote zone. Cleanup the node
 	// from the local zone cache.
 	_, present := oc.localZoneNodes.Load(node.Name)
 
 	if present {
-		klog.Infof("Node %q moved from the local zone %s to a remote zone %s. Deleting it locally", node.Name, oc.zone, util.GetNodeZone(node))
-		if err := oc.deleteNodeEvent(node); err != nil {
-			return fmt.Errorf("error deleting the remote node %s, err : %w", node.Name, err)
+		klog.Infof("Node %q moved from the local zone %s to a remote zone %s. Cleaning the node resources", node.Name, oc.zone, util.GetNodeZone(node))
+		if err := oc.cleanupNodeResources(node.Name); err != nil {
+			return fmt.Errorf("error cleaning up the local resources for the remote node %s, err : %w", node.Name, err)
 		}
 	}
-	return nil
+
+	var err error
+	if syncZoneIC && config.OVNKubernetesFeature.EnableInterconnect {
+		// Call zone chassis handler's AddRemoteZoneNode function to creates
+		// the remote chassis for the remote zone node node in the SB DB or mark
+		// the entry as remote if it was local chassis earlier
+		if err = oc.zoneChassisHandler.AddRemoteZoneNode(node); err != nil {
+			err = fmt.Errorf("adding or updating remote node %s failed, err - %w", node.Name, err)
+			oc.syncZoneICFailed.Store(node.Name, true)
+		} else {
+			// Call zone IC handler's AddRemoteZoneNode function to create
+			// interconnect resources in the OVN Northbound db for this remote zone node.
+			if err = oc.zoneICHandler.AddRemoteZoneNode(node); err != nil {
+				err = fmt.Errorf("adding or updating remote node %s failed, err - %w", node.Name, err)
+				oc.syncZoneICFailed.Store(node.Name, true)
+			} else {
+				oc.syncZoneICFailed.Delete(node.Name)
+			}
+		}
+	}
+
+	return err
 }
 
 func (oc *DefaultNetworkController) deleteNodeEvent(node *kapi.Node) error {
@@ -862,8 +919,20 @@ func (oc *DefaultNetworkController) deleteNodeEvent(node *kapi.Node) error {
 		}
 	}
 
-	if err := oc.deleteNode(node.Name); err != nil {
+	if err := oc.cleanupNodeResources(node.Name); err != nil {
 		return err
+	}
+
+	if config.OVNKubernetesFeature.EnableInterconnect {
+		if err := oc.zoneICHandler.DeleteNode(node); err != nil {
+			return err
+		}
+		if !oc.isLocalZoneNode(node) {
+			if err := oc.zoneChassisHandler.DeleteRemoteZoneNode(node); err != nil {
+				return err
+			}
+		}
+		oc.syncZoneICFailed.Delete(node.Name)
 	}
 
 	oc.lsManager.DeleteSwitch(node.Name)
@@ -872,6 +941,7 @@ func (oc *DefaultNetworkController) deleteNodeEvent(node *kapi.Node) error {
 	oc.gatewaysFailed.Delete(node.Name)
 	oc.nodeClusterRouterPortFailed.Delete(node.Name)
 	oc.localZoneNodes.Delete(node.Name)
+
 	return nil
 }
 

go-controller/pkg/libovsdb/libovsdb.go
@@ -113,6 +113,8 @@ func NewSBClientWithConfig(cfg config.OvnAuthConfig, promRegistry prometheus.Reg
 			client.WithTable(&sbdb.MACBinding{}),
 			// used by node sync
 			client.WithTable(&sbdb.Chassis{}),
+			// used by zone interconnect
+			client.WithTable(&sbdb.Encap{}),
 			// used by node sync, only interested in names
 			client.WithTable(&chassisPrivate, &chassisPrivate.Name),
 			// used by node sync, only interested in Chassis reference

go-controller/pkg/libovsdbops/chassis.go
@@ -28,6 +28,26 @@ func ListChassisPrivate(sbClient libovsdbclient.Client) ([]*sbdb.ChassisPrivate,
 	return found, err
 }
 
+// GetChassis looks up a chassis from the cache using the 'Name' column which is an indexed
+// column.
+func GetChassis(sbClient libovsdbclient.Client, chassis *sbdb.Chassis) (*sbdb.Chassis, error) {
+	found := []*sbdb.Chassis{}
+	opModel := operationModel{
+		Model:          chassis,
+		ExistingResult: &found,
+		ErrNotFound:    true,
+		BulkOp:         false,
+	}
+
+	m := newModelClient(sbClient)
+	err := m.Lookup(opModel)
+	if err != nil {
+		return nil, err
+	}
+
+	return found[0], nil
+}
+
 // DeleteChassis deletes the provided chassis and associated private chassis
 func DeleteChassis(sbClient libovsdbclient.Client, chassis ...*sbdb.Chassis) error {
 	opModels := make([]operationModel, 0, len(chassis))
@@ -114,4 +134,33 @@ func DeleteChassisWithPredicate(sbClient libovsdbclient.Client, p chassisPredica
 	m := newModelClient(sbClient)
 	err := m.Delete(opModels...)
 	return err
+}
+
+// CreateOrUpdateChassis creates or updates the chassis record along with the encap record
+func CreateOrUpdateChassis(sbClient libovsdbclient.Client, chassis *sbdb.Chassis, encap *sbdb.Encap) error {
+	m := newModelClient(sbClient)
+	opModels := []operationModel{
+		{
+			Model: encap,
+			DoAfter: func() {
+				encaps := append(chassis.Encaps, encap.UUID)
+				chassis.Encaps = sets.New(encaps...).UnsortedList()
+			},
+			OnModelUpdates: onModelUpdatesAllNonDefault(),
+			ErrNotFound:    false,
+			BulkOp:         false,
+		},
+		{
+			Model:          chassis,
+			OnModelUpdates: onModelUpdatesAllNonDefault(),
+			ErrNotFound:    false,
+			BulkOp:         false,
+		},
+	}
+
+	if _, err := m.CreateOrUpdate(opModels...); err != nil {
+		return err
+	}
+
+	return nil
 }

go-controller/pkg/libovsdbops/model.go
@@ -56,6 +56,10 @@ func getUUID(model model.Model) string {
 		return t.UUID
 	case *sbdb.IGMPGroup:
 		return t.UUID
+	case *sbdb.Encap:
+		return t.UUID
+	case *sbdb.PortBinding:
+		return t.UUID
 	case *sbdb.MACBinding:
 		return t.UUID
 	case *sbdb.SBGlobal:
@@ -112,6 +116,10 @@ func setUUID(model model.Model, uuid string) {
 	case *sbdb.ChassisPrivate:
 		t.UUID = uuid
 	case *sbdb.IGMPGroup:
+		t.UUID = uuid
+	case *sbdb.Encap:
+		t.UUID = uuid
+	case *sbdb.PortBinding:
 		t.UUID = uuid
 	case *sbdb.MACBinding:
 		t.UUID = uuid
@@ -231,6 +239,19 @@ func copyIndexes(model model.Model) model.Model {
 		return &sbdb.IGMPGroup{
 			UUID: t.UUID,
 		}
+	case *sbdb.Encap:
+		return &sbdb.Encap{
+			UUID: t.UUID,
+			Type: t.Type,
+			IP:   t.IP,
+		}
+	case *sbdb.PortBinding:
+		return &sbdb.PortBinding{
+			UUID:        t.UUID,
+			LogicalPort: t.LogicalPort,
+			Datapath:    t.Datapath,
+			TunnelKey:   t.TunnelKey,
+		}
 	case *sbdb.MACBinding:
 		return &sbdb.MACBinding{
 			UUID:        t.UUID,
@@ -299,6 +320,10 @@ func getListFromModel(model model.Model) interface{} {
 		return &[]*sbdb.ChassisPrivate{}
 	case *sbdb.IGMPGroup:
 		return &[]*sbdb.IGMPGroup{}
+	case *sbdb.Encap:
+		return &[]*sbdb.Encap{}
+	case *sbdb.PortBinding:
+		return &[]*sbdb.PortBinding{}
 	case *sbdb.MACBinding:
 		return &[]*sbdb.MACBinding{}
 	case *nbdb.QoS:

go-controller/pkg/libovsdbops/portbinding.go
@@ -1,0 +1,34 @@
+package libovsdbops
+
+import (
+	"fmt"
+
+	libovsdbclient "github.com/ovn-org/libovsdb/client"
+
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/sbdb"
+)
+
+// UpdatePortBindingSetChassis sets the chassis column of the 'portBinding' row so that the OVN thinks that
+// the port binding 'portBinding' is bound on the chassis. Ideally its ovn-controller which claims/binds
+// a port binding. But for a remote chassis, we have to bind it as we created the remote chassis
+// record for the remote zone nodes.
+// TODO (numans) remove this function once OVN supports binding a port binding for a remote
+// chassis.
+func UpdatePortBindingSetChassis(sbClient libovsdbclient.Client, portBinding *sbdb.PortBinding, chassis *sbdb.Chassis) error {
+	ch, err := GetChassis(sbClient, chassis)
+	if err != nil {
+		return fmt.Errorf("failed to get chassis id %s(%s), error: %v", chassis.Name, chassis.Hostname, err)
+	}
+	portBinding.Chassis = &ch.UUID
+
+	opModel := operationModel{
+		Model:          portBinding,
+		OnModelUpdates: []interface{}{&portBinding.Chassis},
+		ErrNotFound:    true,
+		BulkOp:         false,
+	}
+
+	m := newModelClient(sbClient)
+	_, err = m.CreateOrUpdate(opModel)
+	return err
+}

go-controller/pkg/node/default_node_network_controller.go
@@ -290,6 +290,9 @@ func setupOVNNode(node *kapi.Node) error {
 		fmt.Sprintf("other_config:bundle-idle-timeout=%d",
 			config.Default.OpenFlowProbe),
 		fmt.Sprintf("external_ids:hostname=\"%s\"", node.Name),
+		// If Interconnect feature is enabled, we want to tell ovn-controller to
+		// make this node/chassis as an interconnect gateway.
+		fmt.Sprintf("external_ids:ovn-is-interconn=%s", strconv.FormatBool(config.OVNKubernetesFeature.EnableInterconnect)),
 		fmt.Sprintf("external_ids:ovn-monitor-all=%t", config.Default.MonitorAll),
 		fmt.Sprintf("external_ids:ovn-ofctrl-wait-before-clear=%d", config.Default.OfctrlWaitBeforeClear),
 		fmt.Sprintf("external_ids:ovn-enable-lflow-cache=%t", config.Default.LFlowCacheEnable),

go-controller/pkg/node/default_node_network_controller_test.go
@@ -253,6 +253,7 @@ var _ = Describe("Node", func() {
 						"external_ids:ovn-openflow-probe-interval=%d "+
 						"other_config:bundle-idle-timeout=%d "+
 						"external_ids:hostname=\"%s\" "+
+						"external_ids:ovn-is-interconn=false "+
 						"external_ids:ovn-monitor-all=true "+
 						"external_ids:ovn-ofctrl-wait-before-clear=0 "+
 						"external_ids:ovn-enable-lflow-cache=true",
@@ -315,6 +316,7 @@ var _ = Describe("Node", func() {
 						"external_ids:ovn-openflow-probe-interval=%d "+
 						"other_config:bundle-idle-timeout=%d "+
 						"external_ids:hostname=\"%s\" "+
+						"external_ids:ovn-is-interconn=false "+
 						"external_ids:ovn-monitor-all=true "+
 						"external_ids:ovn-ofctrl-wait-before-clear=0 "+
 						"external_ids:ovn-enable-lflow-cache=true",
@@ -390,6 +392,7 @@ var _ = Describe("Node", func() {
 						"external_ids:ovn-openflow-probe-interval=%d "+
 						"other_config:bundle-idle-timeout=%d "+
 						"external_ids:hostname=\"%s\" "+
+						"external_ids:ovn-is-interconn=false "+
 						"external_ids:ovn-monitor-all=true "+
 						"external_ids:ovn-ofctrl-wait-before-clear=0 "+
 						"external_ids:ovn-enable-lflow-cache=false "+
@@ -457,6 +460,7 @@ var _ = Describe("Node", func() {
 						"external_ids:ovn-openflow-probe-interval=%d "+
 						"other_config:bundle-idle-timeout=%d "+
 						"external_ids:hostname=\"%s\" "+
+						"external_ids:ovn-is-interconn=false "+
 						"external_ids:ovn-monitor-all=true "+
 						"external_ids:ovn-ofctrl-wait-before-clear=0 "+
 						"external_ids:ovn-enable-lflow-cache=true",
@@ -530,6 +534,7 @@ var _ = Describe("Node", func() {
 						"external_ids:ovn-openflow-probe-interval=%d "+
 						"other_config:bundle-idle-timeout=%d "+
 						"external_ids:hostname=\"%s\" "+
+						"external_ids:ovn-is-interconn=false "+
 						"external_ids:ovn-monitor-all=true "+
 						"external_ids:ovn-ofctrl-wait-before-clear=0 "+
 						"external_ids:ovn-enable-lflow-cache=true",
@@ -603,6 +608,7 @@ var _ = Describe("Node", func() {
 						"external_ids:ovn-openflow-probe-interval=%d "+
 						"other_config:bundle-idle-timeout=%d "+
 						"external_ids:hostname=\"%s\" "+
+						"external_ids:ovn-is-interconn=false "+
 						"external_ids:ovn-monitor-all=true "+
 						"external_ids:ovn-ofctrl-wait-before-clear=0 "+
 						"external_ids:ovn-enable-lflow-cache=true",

go-controller/pkg/ovn/default_network_controller.go
@@ -24,6 +24,7 @@ import (
 	aclsyncer "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/external_ids_syncer/acl"
 	addrsetsyncer "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/external_ids_syncer/address_set"
 	lsm "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/logical_switch_manager"
+	zoneic "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/zone_interconnect"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/retry"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/syncmap"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/types"
@@ -120,6 +121,7 @@ type DefaultNetworkController struct {
 	addNodeFailed               sync.Map
 	nodeClusterRouterPortFailed sync.Map
 	hybridOverlayFailed         sync.Map
+	syncZoneICFailed            sync.Map
 
 	// retry framework for Cloud private IP config
 	retryCloudPrivateIPConfig *retry.RetryFramework
@@ -131,6 +133,14 @@ type DefaultNetworkController struct {
 	// IP addresses of OVN Cluster logical router port ("GwRouterToJoinSwitchPrefix + OVNClusterRouter")
 	// connecting to the join switch
 	ovnClusterLRPToJoinIfAddrs []*net.IPNet
+
+	// zoneICHandler creates the interconnect resources for local nodes and remote nodes.
+	// Interconnect resources are Transit switch and logical ports connecting this transit switch
+	// to the cluster router. Please see zone_interconnect/interconnect_handler.go for more details.
+	zoneICHandler *zoneic.ZoneInterconnectHandler
+	// zoneChassisHandler handles the local node and remote nodes in creating or updating the chassis entries in the OVN Southbound DB.
+	// Please see zone_interconnect/chassis_handler.go for more details.
+	zoneChassisHandler *zoneic.ZoneChassisHandler
 }
 
 // NewDefaultNetworkController creates a new OVN controller for creating logical network
@@ -157,6 +167,14 @@ func newDefaultNetworkControllerCommon(cnci *CommonNetworkControllerInfo,
 	if err != nil {
 		return nil, fmt.Errorf("unable to create new egress service controller while creating new default network controller: %w", err)
 	}
+
+	var zoneICHandler *zoneic.ZoneInterconnectHandler
+	var zoneChassisHandler *zoneic.ZoneChassisHandler
+	if config.OVNKubernetesFeature.EnableInterconnect {
+		zoneICHandler = zoneic.NewZoneInterconnectHandler(&util.DefaultNetInfo{}, cnci.nbClient, cnci.sbClient)
+		zoneChassisHandler = zoneic.NewZoneChassisHandler(cnci.sbClient)
+	}
+
 	oc := &DefaultNetworkController{
 		BaseNetworkController: BaseNetworkController{
 			CommonNetworkControllerInfo: *cnci,
@@ -197,6 +215,8 @@ func newDefaultNetworkControllerCommon(cnci *CommonNetworkControllerInfo,
 		svcController:                svcController,
 		svcFactory:                   svcFactory,
 		egressSvcController:          egressSvcController,
+		zoneICHandler:                zoneICHandler,
+		zoneChassisHandler:           zoneChassisHandler,
 	}
 
 	// Allocate IPs for logical router port "GwRouterToJoinSwitchPrefix + OVNClusterRouter". This should always
@@ -696,14 +716,16 @@ func (h *defaultNetworkControllerEventHandler) AddResource(obj interface{}, from
 				_, mgmtSync := h.oc.mgmtPortFailed.Load(node.Name)
 				_, gwSync := h.oc.gatewaysFailed.Load(node.Name)
 				_, hoSync := h.oc.hybridOverlayFailed.Load(node.Name)
+				_, zoneICSync := h.oc.syncZoneICFailed.Load(node.Name)
 				nodeParams = &nodeSyncs{
 					nodeSync,
 					clusterRtrSync,
 					mgmtSync,
 					gwSync,
-					hoSync}
+					hoSync,
+					zoneICSync}
 			} else {
-				nodeParams = &nodeSyncs{true, true, true, true, config.HybridOverlay.Enabled}
+				nodeParams = &nodeSyncs{true, true, true, true, config.HybridOverlay.Enabled, config.OVNKubernetesFeature.EnableInterconnect}
 			}
 
 			if err = h.oc.addUpdateLocalNodeEvent(node, nodeParams); err != nil {
@@ -712,7 +734,7 @@ func (h *defaultNetworkControllerEventHandler) AddResource(obj interface{}, from
 				return err
 			}
 		} else {
-			if err = h.oc.addUpdateRemoteNodeEvent(node); err != nil {
+			if err = h.oc.addUpdateRemoteNodeEvent(node, config.OVNKubernetesFeature.EnableInterconnect); err != nil {
 				return err
 			}
 		}
@@ -849,22 +871,27 @@ func (h *defaultNetworkControllerEventHandler) UpdateResource(oldObj, newObj int
 					nodeSubnetChanged(oldNode, newNode) || hostAddressesChanged(oldNode, newNode) ||
 					nodeGatewayMTUSupportChanged(oldNode, newNode))
 				_, hoSync := h.oc.hybridOverlayFailed.Load(newNode.Name)
+				_, syncZoneIC := h.oc.syncZoneICFailed.Load(newNode.Name)
 				nodeSyncsParam = &nodeSyncs{
 					nodeSync,
 					clusterRtrSync,
 					mgmtSync,
 					gwSync,
-					hoSync}
+					hoSync,
+					syncZoneIC}
 			} else {
 				klog.Infof("Node %s moved from the remote zone %s to local zone.",
 					newNode.Name, util.GetNodeZone(oldNode), util.GetNodeZone(newNode))
 				// The node is now a local zone node.  Trigger a full node sync.
-				nodeSyncsParam = &nodeSyncs{true, true, true, true, true}
+				nodeSyncsParam = &nodeSyncs{true, true, true, true, true, config.OVNKubernetesFeature.EnableInterconnect}
 			}
 
 			return h.oc.addUpdateLocalNodeEvent(newNode, nodeSyncsParam)
 		} else {
-			return h.oc.addUpdateRemoteNodeEvent(newNode)
+			_, syncZoneIC := h.oc.syncZoneICFailed.Load(newNode.Name)
+			// Check if the node moved from local zone to remote zone and if so syncZoneIC should be set to true
+			syncZoneIC = syncZoneIC || h.oc.isLocalZoneNode(oldNode)
+			return h.oc.addUpdateRemoteNodeEvent(newNode, syncZoneIC)
 		}
 
 	case factory.EgressIPType:

commit 52583b3c3287b5925f6fabe0b212e1c54702af92

network controller: Remove the node name from local zone nodes cache.
When a node moves from a local zone to a remote zone, remove the
node name from the default network controller's 'localZoneNodes' Sync
Map. Without this ovnkube network controller will be in a continuous
retry loop as the next attempt to cleanup the node resources will fail.
Also ignore the error in cleanup.  Its possible that a node may move zones
even before all the resources (like node router port connecting to the
GW router) are created.

Fixes: 8aa14239e4b1("network-controller-manager: Create interconnect resources.")

Signed-off-by: Numan Siddique <numans@ovn.org>

go-controller/pkg/ovn/master.go
@@ -866,8 +866,13 @@ func (oc *DefaultNetworkController) addUpdateRemoteNodeEvent(node *kapi.Node, sy
 	if present {
 		klog.Infof("Node %q moved from the local zone %s to a remote zone %s. Cleaning the node resources", node.Name, oc.zone, util.GetNodeZone(node))
 		if err := oc.cleanupNodeResources(node.Name); err != nil {
-			return fmt.Errorf("error cleaning up the local resources for the remote node %s, err : %w", node.Name, err)
+			// Don't return error if the cleanup fails. Instead log a warning
+			// Otherwise we will be in a retry loop trying to cleanup.
+			// cleanupNodeResources can fail when a node moves from a local zone to the remote zone before
+			// the node resources are created completely.
+			klog.Warningf("Error cleaning up the local resources for the remote node %s, err : %w", node.Name, err)
 		}
+		oc.localZoneNodes.Delete(node.Name)
 	}
 
 	var err error